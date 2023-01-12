1a732aa7148702e724319c228a85bbe97ceec03b
sarvagna <sarvagnakadiya1@gmail.com>
2023-01-12 18:58:38 +0530

    comments and confirmation added

--- src/IDADemo.js
+++ src/IDADemo.js
@@ -7,21 +7,30 @@ const IDADemo = () => {
   //---------All useStates for IDA
   const [id, setID] = useState("");
   const [subscriberData, setSubscriberData] = useState([]);
+
   const { address } = useAccount();
   const { chain } = useNetwork();
 
-  //   console.log(address);
-  //   console.log(chain);
   const provider = useProvider();
   const { data: signer } = useSigner();
 
-  console.log(subscriberData.subscriberAddress);
-  console.log(subscriberData.subscriberUnits);
+  //   console.log(address);
+  //   console.log(chain);
+  //   console.log(subscriberData.subscriberAddress);
+  //   console.log(subscriberData.subscriberUnits);
 
-  //------------------to create Index using superfluid' SDK core------------------
-  const createIndexSf = async () => {
-    // const id = "3";
-    console.log("Index creating");
+  //------------------Function to create Index---------------------------
+  /*
+  Must be call by publisher
+  params:
+    indexID(int)       //choise of your own (must be unique)
+    and signer to sign the transaction
+
+  returns:
+    log(indexId)
+   */
+  const createIndex = async () => {
+    console.log("Inside createIndex() function");
     const sf = await Framework.create({
       chainId: 5,
       provider: provider,
@@ -31,7 +40,7 @@ const IDADemo = () => {
       const createIndexOperation = daix.createIndex({
         indexId: id.toString(),
       });
-      console.log("Creating your Index...");
+      console.log(`Creating index ID:${id}...`);
 
       const sign = await createIndexOperation.exec(signer);
       const receipt = await sign.wait(sign);
@@ -48,9 +57,21 @@ const IDADemo = () => {
       console.log(error);
     }
   };
-  //------------------to create Index using superfluid' SDK core------------------
+  //------------------Function to add subscriber to the given Index------------------
+  /*
+    Must be called by publisher
+    params:
+        indexID(int)       //already created
+        subscriber(address)   //address of thr subscriber which you want to add
+        units(int)
+        and signer to sign the transaction
+
+    returns:
+        log(subscriber address,uints & indexId which its subscribing to)
+    */
   const addSubscriber = async () => {
-    console.log("Adding subscriber");
+    console.log("Inside addSubscriber() function");
+
     const sf = await Framework.create({
       chainId: 5,
       provider: provider,
@@ -62,7 +83,9 @@ const IDADemo = () => {
         subscriber: subscriberData.subscriberAddress,
         units: subscriberData.subscriberUnits,
       });
-      console.log("Adding the subscriber...");
+      console.log(
+        `Adding ${subscriberData.subscriberAddress} as subscriber...`
+      );
 
       const sign = await createIndexOperation.exec(signer);
       const receipt = await sign.wait(sign);
@@ -76,9 +99,21 @@ const IDADemo = () => {
     }
   };
 
-  //gets the subscription data
-  //can be called by publisher to check subscription of a perticular subscriber
-  // returns(approved(bool), exists(bool), pendingDistribution(int), units(int))
+  //------------------Function to get subscription details of a perticular subscriber------------------
+  /*
+    Must be called by publisher
+    params:
+        publisher(address)      //address of publisher(who created the IndexId)
+        indexID(int)       //already created
+        subscriber(address)   //address of thr subscriber which you want to add
+        procider/signer
+
+    returns:
+        approved(bool), 
+        exists(bool), 
+        pendingDistribution(int), 
+        units(int)
+    */
   const getSubscription = async () => {
     console.log("Getting  subscription data");
     const sf = await Framework.create({
@@ -104,11 +139,27 @@ const IDADemo = () => {
   called by publisher
   returns(
     exists(bool),
-    indexValue(int),    //value of index
+    indexValue(int),                //value of index
     totalUnitsApproved(int),        //units approved    
     totalUnitsPending(int)          //units pending to be approved
     )
  */
+
+  //------------------Function to get indexId details of a perticular indexId------------------
+  /*
+        Must be called by publisher
+        params:
+            publisher(address)      //address of publisher(who created the IndexId)
+            indexID(int)       //already created
+            procider/signer
+
+        returns
+            exists(bool),
+            indexValue(int),                //value of index
+            totalUnitsApproved(int),        //units approved    
+            totalUnitsPending(int)          //units pending to be approved
+    
+    */
   const getIndex = async () => {
     console.log("Getting Index data");
     const sf = await Framework.create({
@@ -127,8 +178,21 @@ const IDADemo = () => {
       console.log(error);
     }
   };
+
+  //------------------Function to update Index value of any perticular IndexId------------------
+  /*
+    Must be called by publisher
+    params:
+        indexID(string)       //already created
+        value(string)       //value you want to send for a distribution(Adds the previous value with given value)
+        and signer to sign the transaction
+
+    returns:
+        log(indexId to which value updated)
+    */
   const updateIndexValue = async () => {
-    console.log("Update Index Value");
+    console.log("Inside updateIndexValue() function");
+
     const sf = await Framework.create({
       chainId: 5,
       provider: provider,
@@ -144,21 +208,31 @@ const IDADemo = () => {
       const sign = await createIndexOperation.exec(signer);
       const receipt = await sign.wait(sign);
       if (receipt) {
-        console.log(`Value Update for Index ID: ${id}`);
+        console.log(`Value is Updated for the Index ID: ${id}`);
       }
     } catch (error) {
       console.log(error);
     }
   };
-  const ida = async () => {
+
+  //------------------Function to Approve subscription------------------
+  /*
+    Must be called by subscriber
+    params:
+        indexID(string)       //already created
+        publisher(address)      //address of publisher(who created the IndexId)
+
+    returns:
+        log(Confirmation)
+    */
+  const approveSubscription = async () => {
     const sf = await Framework.create({
       chainId: 5,
       provider: provider,
     });
-    //daix token loading
+
     const daix = await sf.loadSuperToken("fDAIx");
     try {
-      //---------------------------approveSubscription
       const subscribeOperation = daix.approveSubscription({
         indexId: id,
         publisher: "0xcc920c851327AF767b4bf770e3b2C2ea50B90fde",
@@ -169,17 +243,21 @@ const IDADemo = () => {
         console.log("approved!");
       }
     } catch (err) {
-      if (
-        err.errorObject.errorObject.error.reason ==
-        "execution reverted: IDA: E_SUBS_APPROVED"
-      ) {
-        console.log("shareGainer already approved subscription. moving on ->");
-      }
+      console.log(err);
     }
   };
 
-  //-----revokes the approved units, but stays as a subscriber
-  const revokeIda = async () => {
+  //------------------Function to revoke subscription------------------
+  /*
+    Must be called by subscriber
+    params:
+        indexID(string)       //already created
+        publisher(address)      //address of publisher(who created the IndexId)
+
+    returns:
+        log(Confirmation)
+    */
+  const revokeSubscription = async () => {
     const sf = await Framework.create({
       chainId: 5,
       provider: provider,
@@ -201,15 +279,26 @@ const IDADemo = () => {
       console.log(err);
     }
   };
+
+  //------------------Function to delete subscriber------------------
+  /*
+    Must be called by publisher
+    params:
+        indexID(string)       //already created
+        subscriber(address)   //address of the subscriber which you want to delete
+        publisher(address)      //address of publisher(who created the IndexId)
+
+    returns:
+        log(Confirmation)
+    */
   const deleteSubscription = async () => {
     const sf = await Framework.create({
       chainId: 5,
       provider: provider,
     });
-    //daix token loading
+
     const daix = await sf.loadSuperToken("fDAIx");
     try {
-      //---------------------------revoke Subscription-------------------
       const subscribeOperation = daix.deleteSubscription({
         indexId: id,
         subscriber: subscriberData.subscriberAddress,
@@ -224,15 +313,26 @@ const IDADemo = () => {
       console.log(err);
     }
   };
+
+  //------------------Function to Distribute Funds------------------
+  /*
+    Must be called by publisher
+    params:
+        indexID(string)       //already created
+        amount(string)         //amount which you want to distribute while calling this function
+        require -> amount < index Value
+
+    returns:
+        log(Confirmation)
+    */
   const distributeFunds = async () => {
     const sf = await Framework.create({
       chainId: 5,
       provider: provider,
     });
-    //daix token loading
+
     const daix = await sf.loadSuperToken("fDAIx");
     try {
-      //---------------------------Distribute funds-------------------
       const subscribeOperation = daix.distribute({
         indexId: id,
         amount: "100000000000000000000",
@@ -246,15 +346,27 @@ const IDADemo = () => {
       console.log(err);
     }
   };
+
+  //------------------Function to Claim Funds(it works like approveSubscription function, but it can directly claim it)------------------
+  /*
+    Must be called by subscriber
+    cant be used if subscriber has already done approve the IndexId
+    params:
+        indexID(string)       //already created
+        subscriber(address)   //Your address
+        publisher(address)      //address of publisher(who created the IndexId)
+
+    returns:
+        log(Confirmation)
+    */
   const claimFunds = async () => {
     const sf = await Framework.create({
       chainId: 5,
       provider: provider,
     });
-    //daix token loading
+
     const daix = await sf.loadSuperToken("fDAIx");
     try {
-      //---------------------------Distribute funds-------------------
       console.log("claiming funds");
       const subscribeOperation = daix.claim({
         indexId: id,
@@ -273,7 +385,7 @@ const IDADemo = () => {
   return (
     <div>
       <input type="text" onChange={(e) => setID(e.target.value)}></input>
-      <button onClick={() => createIndexSf()}>Create Index</button>
+      <button onClick={() => createIndex()}>Create Index</button>
       <br></br>
       <input
         type="text"
@@ -297,8 +409,8 @@ const IDADemo = () => {
       <button onClick={() => getSubscription()}>get Subscription</button>
       <button onClick={() => getIndex()}>get index</button>
       <button onClick={() => updateIndexValue()}>update value</button>
-      <button onClick={() => ida()}>IDA</button>
-      <button onClick={() => revokeIda()}>revoke IDA</button>
+      <button onClick={() => approveSubscription()}>IDA</button>
+      <button onClick={() => revokeSubscription()}>revoke IDA</button>
       <button onClick={() => deleteSubscription()}>Delete subscriber</button>
       <button onClick={() => distributeFunds()}>Distribute funds</button>
       <button onClick={() => claimFunds()}>claim funds</button>
